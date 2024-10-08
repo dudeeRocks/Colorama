4bc70ad5d4f46e8e4f8702b564e15047692b624b
David Katsman <kadamc90@gmail.com>
2024-10-08 15:22:03 +0300

    initial commit

---
+++ ColoramaSwiftUI/ColoramaSwiftUI/ColoramaSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColoramaSwiftUIApp.swift
+//  ColoramaSwiftUI
+//
+//  Created by David Katsman on 08/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct ColoramaSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
