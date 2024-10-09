9c7f206c06157b256f6fbc1f3aa22d6624dbc77b
David Katsman <kadamc90@gmail.com>
2024-10-09 09:38:54 +0300

    Added fullscreen support on details view

--- ColoramaSwiftUI/ColoramaSwiftUI/ColorDetailsView.swift
+++ ColoramaSwiftUI/ColoramaSwiftUI/ColorDetailsView.swift
@@ -6,16 +6,44 @@ struct ColorDetailsView: View {
     
     @Binding var color: ColorItem
     
+    @State private var isFullscreen: Bool = false
+    @Namespace private var namespace
+    
     var body: some View {
-        VStack {
-            RoundedRectangle(cornerRadius: 10)
-                .fill(color.color)
-                .frame(height: 380)
-            Text(color.name)
-                .font(.largeTitle)
-                .bold()
-            Spacer()
+        if isFullscreen {
+            ZStack {
+                RoundedRectangle(cornerRadius: 0)
+                    .fill(color.color)
+                    .matchedGeometryEffect(id: color.id, in: namespace, properties: .frame, anchor: .top, isSource: false)
+                Text(color.name)
+                    .font(.largeTitle)
+                    .foregroundStyle(.background)
+                    .bold()
+                    .matchedGeometryEffect(id: color.name, in: namespace, properties: .position, anchor: .center, isSource: false)
+            }
+            .onTapGesture {
+                withAnimation(.easeInOut) {
+                    isFullscreen = false
+                }
+            }
+        } else {
+            VStack {
+                RoundedRectangle(cornerRadius: 10)
+                    .fill(color.color)
+                    .frame(height: 380)
+                    .matchedGeometryEffect(id: color.id, in: namespace, properties: .frame, anchor: .top, isSource: true)
+                    .onTapGesture {
+                        withAnimation(.easeInOut) {
+                            isFullscreen = true
+                        }
+                    }
+                Text(color.name)
+                    .font(.largeTitle)
+                    .bold()
+                    .matchedGeometryEffect(id: color.name, in: namespace, properties: .position, anchor: .center, isSource: true)
+                Spacer()
+            }
+            .padding()
         }
-        .padding()
     }
 }
