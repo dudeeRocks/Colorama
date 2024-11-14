f679dd7bea086cb9b2fb7fc1fc3516e437c66544
David Katsman <kadamc90@gmail.com>
2024-11-14 15:13:26 +0200

    Implemented Color Content View

---
+++ ColoramaUIKit/ColoramaUIKit/ViewControllers/ColorDetails/ColorDetailsViewController+CellRegistration.swift
@@ -0,0 +1,49 @@
+// Abstract: methods to handle cell registration for the collection view of the details screen
+
+import UIKit
+
+extension ColorDetailsViewController {
+    func registerCell() {
+        cellRegistration = .init(handler: cellRegistrationHandler)
+    }
+    
+    private func cellRegistrationHandler(for cell: UICollectionViewListCell, at indexPath: IndexPath, item: SectionItem) {        
+        switch item {
+        case .sectionHeader(let title):
+            if !title.isEmpty {
+                configureSectionHeader(for: cell, title: title)
+            }
+        case .color(let color):
+            configureColorView(for: cell, color: color)
+        case .name(let name):
+            configureNameView(for: cell, name: name)
+        case .deleteButton:
+            configureDeleteButton(for: cell)
+        }
+    }
+    
+    private func configureSectionHeader(for cell: UICollectionViewListCell, title: String) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = title
+        cell.contentConfiguration = contentConfiguration
+    }
+    
+    private func configureColorView(for cell: UICollectionViewListCell, color: UIColor) {
+        var contentConfiguration = ColorView.Configuration()
+        contentConfiguration.color = color
+        cell.contentConfiguration = contentConfiguration
+    }
+    
+    private func configureNameView(for cell: UICollectionViewListCell, name: String) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = name
+        cell.contentConfiguration = contentConfiguration
+    }
+    
+    private func configureDeleteButton(for cell: UICollectionViewListCell) {
+        var contentConfiguration = cell.defaultContentConfiguration()
+        contentConfiguration.text = "Delete"
+        contentConfiguration.textProperties.color = .systemRed
+        cell.contentConfiguration = contentConfiguration
+    }
+}
---
+++ ColoramaUIKit/ColoramaUIKit/Views/ColorDetails/ColorView.swift
@@ -0,0 +1,54 @@
+// Abstract: content view for color on details screen
+
+import UIKit
+
+class ColorView: UIView, UIContentView {
+    
+    var configuration: any UIContentConfiguration {
+        didSet { configure(configuration) }
+    }
+    
+    private let colorView = UIView()
+    
+    private func configure(_ configuration: any UIContentConfiguration) {
+        guard let configuration = configuration as? Configuration else { return }
+        colorView.backgroundColor = configuration.color
+    }
+    
+    private func layOutViews() {
+        addSubview(colorView)
+        colorView.translatesAutoresizingMaskIntoConstraints = false
+        
+        NSLayoutConstraint.activate([
+            colorView.topAnchor.constraint(equalTo: topAnchor),
+            colorView.leadingAnchor.constraint(equalTo: leadingAnchor),
+            colorView.trailingAnchor.constraint(equalTo: trailingAnchor),
+            colorView.bottomAnchor.constraint(equalTo: bottomAnchor),
+            colorView.widthAnchor.constraint(equalTo: widthAnchor),
+            colorView.heightAnchor.constraint(equalTo: widthAnchor)
+        ])
+    }
+    
+    init(configuration: any UIContentConfiguration) {
+        self.configuration = configuration
+        super.init(frame: .zero)
+        layOutViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    struct Configuration: UIContentConfiguration {
+        
+        var color: UIColor!
+        
+        func makeContentView() -> any UIView & UIContentView {
+            ColorView(configuration: self)
+        }
+        
+        func updated(for state: any UIConfigurationState) -> ColorView.Configuration {
+            self
+        }
+    }
+}
