64ec5e7c0252ab19725be3b2ee208dd740fb3c88
David Katsman <kadamc90@gmail.com>
2024-11-28 16:12:39 +0200

    Added links to articles

--- ColoramaSwiftUI/ColoramaSwiftUI/ColoramaSwiftUIApp.swift
+++ ColoramaSwiftUI/ColoramaSwiftUI/ColoramaSwiftUIApp.swift
@@ -1,9 +1,13 @@
 //
-//  ColoramaSwiftUIApp.swift
+//
 //  ColoramaSwiftUI
+//  Simple app showcasing the ease of implementation of tables and collections in SwiftUI.
 //
-//  Created by David Katsman on 08/10/2024.
-//
+//  Created by David Katsman on 08/10/2024 as a complimentary to the following articles:
+//  1. https://dudee.rocks/2024/11/28/how-to-use-diffable-data-source-in-uikit/
+//  2. https://dudee.rocks/2024/11/28/how-to-use-diffable-data-source-with-uitableview/
+//  3. https://dudee.rocks/2024/11/28/how-to-create-grid-layout-in-uikit/
+//  4. https://dudee.rocks/2024/11/28/how-to-use-uicontentview-in-uikit/
 
 import SwiftUI
 
--- ColoramaUIKit/ColoramaUIKit/AppDelegate.swift
+++ ColoramaUIKit/ColoramaUIKit/AppDelegate.swift
@@ -2,10 +2,10 @@
 //  Simple app to showcase the use of diffable data source in UIKit.
 //
 //  Created by David Katsman on 08/10/2024 as a complimentary to the following articles:
-//  1.
-//  2.
-//  3.
-//  4. 
+//  1. https://dudee.rocks/2024/11/28/how-to-use-diffable-data-source-in-uikit/
+//  2. https://dudee.rocks/2024/11/28/how-to-use-diffable-data-source-with-uitableview/
+//  3. https://dudee.rocks/2024/11/28/how-to-create-grid-layout-in-uikit/
+//  4. https://dudee.rocks/2024/11/28/how-to-use-uicontentview-in-uikit/
 
 import UIKit
 
